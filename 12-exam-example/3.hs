430b49d9a35259e3ba4233d9030f7595cee6e3c5
ivan-ristovic <ivan.ristovic@oracle.com>
2024-06-15 18:11:29 +0200

    [ci-skip] auto-update

--- 12-exam-example/3.hs
+++ 12-exam-example/3.hs
@@ -12,9 +12,49 @@ module Zad3 where
 -- Napisati QuickCheck test za funkcije `fromList` i `toList`.
 
 -- Instancirati Show nad `Stek a` (pretpostaviti da je
--- `a` u klasi Show) tako da se skup ispise u formatu:
+-- `a` u klasi Show) tako da se stek ispise u formatu:
 --       < dno, ... , vrh >
 -- (vrh i dno su elementi steka)
 -- Za prazan stek:  </>
 
 -- Instancirati Functor nad `Stek a`
+
+data Stacc a = Stacc [a]
+
+data Stacc' a = Empty
+              | Stacc' a (Stacc' a)
+
+fromList :: [a] -> Stacc a
+fromList = Stacc
+-- fromList []     = Empty
+-- fromList (x:xs) = Stacc' x (fromList xs) 
+-- fromList = foldr Stacc' Empty 
+
+toList :: Stacc a -> [a]
+toList (Stacc xs) = xs
+-- toList Empty = []
+-- toList (Stacc' x xs) = x : (toList xs)
+
+test_fromList_toList :: (Eq a) => [a] -> Bool
+test_fromList_toList lst = lst == (toList $ fromList lst)
+
+instance (Show a) => Show (Stacc a) where
+    show s = "<" ++ show' s ++ ">"
+        where 
+            show' :: Stacc a -> String
+            show' (Stacc [])     = "/"
+            show' (Stacc [x])    = show x 
+            show' (Stacc (x:xs)) = show x ++ ", " ++ show' (Stacc xs) 
+            
+            -- show' (Stacc []) = "/"
+            -- show' (Stacc xs) = filter (\c -> c `elem` ['[', ']']) $ show xs
+
+
+instance Functor Stacc where
+    fmap f (Stacc xs) = fromList $ fmap f xs 
+
+    -- fmap _ Empty        = Empty
+    -- fmap f (Stacc' x xs) = Stacc (f x) (fmap f xs)
+
+-- Set
+-- da li tip Set moze da bude Functor?
